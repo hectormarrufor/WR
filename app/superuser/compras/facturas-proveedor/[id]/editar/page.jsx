47b665fba10c1cc65ee6d7954e8d49a85fce2e0e
Hector <hectormmarrufor@gmail.com>
2025-07-17 11:57:18 -0400

    resolving import issues

--- app/superuser/compras/facturas-proveedor/[id]/editar/page.jsx
+++ app/superuser/compras/facturas-proveedor/[id]/editar/page.jsx
@@ -1,12 +1,14 @@
 // app/superuser/compras/facturas-proveedor/[id]/editar/page.js
 import { Title, Box } from '@mantine/core';
 import { FacturaProveedorForm } from '../../../componentes/FacturaProveedorForm';
+import { use } from 'react';
 
 
 export default function EditarFacturaProveedorPage({ params }) {
+  const {id} = use(params)
   return (
     <Box>
-      <FacturaProveedorForm facturaId={params.id} />
+      <FacturaProveedorForm facturaId={id} />
     </Box>
   );
 }
