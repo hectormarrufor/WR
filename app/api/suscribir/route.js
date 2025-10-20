a29a0599e687a952bbadb78578f92c6600ec0d36
Hector <hectormmarrufor@gmail.com>
2025-10-20 14:41:14 -0400

    improving media

--- app/api/suscribir/route.js
+++ app/api/suscribir/route.js
@@ -1,11 +1,9 @@
 // app/api/suscribir/route.js
 import db from '@/models';
-import { QueryInterface } from 'sequelize';
 
 export async function POST(req) {
   const body = await req.json();
   const { suscripcion, usuarioId, rol } = body;
-  await QueryInterface.dropTable('PushSubscriptions');
   await db.PushSubscription.upsert({
     endpoint: suscripcion.endpoint,
     keys: suscripcion.keys,
