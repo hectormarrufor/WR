df12ddcad7264af91ff73d8e2a6f57fc61542c8a
Hector <hectormmarrufor@gmail.com>
2026-01-19 13:28:24 -0400

    trabajando en las notificaciones

--- sw.js
+++ sw.js
@@ -15,7 +15,6 @@ self.addEventListener('push', event => {
     body: payload.body || '',
     icon: '/icons/icon-192x192.png',
     badge: '/icons/android-launchericon-96-96.png',
-
     data: {
       url: payload.url || '/', // Tomamos la URL del nivel raíz del JSON
       ...payload.data          // Mantenemos cualquier otra data extra si existiera
@@ -26,25 +25,8 @@ self.addEventListener('push', event => {
   event.waitUntil(self.registration.showNotification(title, options));
 });
 
-self.addEventListener('notificationclick', (event) => {
-  // Close the notification immediately after clicking
+self.addEventListener('notificationclick', event => {
+  console.log('[SW] Notificación clickeada:', event.notification);
   event.notification.close();
-
-  // Retrieve the URL from the data object sent in the payload
-  const targetUrl = event.notification.data?.url || '/';
-
-  event.waitUntil(
-    clients.matchAll({ type: 'window', includeUncontrolled: true }).then((windowClients) => {
-      // Check if a tab with the same origin is already open
-      for (const client of windowClients) {
-        if (client.url === targetUrl && 'focus' in client) {
-          return client.focus();
-        }
-      }
-      // If no tab is open, open a new window to the target URL
-      if (clients.openWindow) {
-        return clients.openWindow(targetUrl);
-      }
-    })
-  );
+  event.waitUntil(clients.openWindow(event.notification.data.url || '/'));
 });
